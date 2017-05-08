6d97bc08e072d74bb3ef62bf269bfd163898bb09
inter-isd <alex.onuferko@mail.ru>
2017-05-08 11:48:55 +0300

    Add files via upload

---
+++ controllers/base/meta-tags.php
@@ -0,0 +1,17 @@
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta itemprop="name" content=""/>
+    <meta itemprop="description" content=""/>
+    <meta name="twitter:site" content=""/>
+    <meta name="twitter:title" content="">
+    <meta name="twitter:description" content=""/>
+    <meta name="twitter:creator" content=""/>
+    <meta name="twitter:domain" content=""/>
+    <meta property="og:type" content="website"/> 
+    <meta property="og:title" content=""/>
+    <meta property="og:description" content=""/>
+    <meta property="og:url" content=""/>
+    <meta property="og:site_name" content=""/>
+    <meta property="og:see_also" content=""/>
